4dbf616264f6b80d68d3bcad8f8329ae049057c0
Genba <flynnher@gmail.com>
2022-12-29 11:10:53 +0900

    props : Tag 끼리 데이터 주고 받기

---
+++ src/study/Alpha.js
@@ -0,0 +1,11 @@
+import MyProps from './MyProps';
+
+function Alpha () {
+  return (
+    <div>
+      <MyProps name="리액트" />
+    </div>
+  )
+}
+
+export default Alpha;
---
+++ src/study/MyProps.js
@@ -0,0 +1,9 @@
+const MyProps = props => {
+  return (
+    <div>
+      안녕하세요, My Name is {props.name||"무 명"}.
+    </div>
+  )
+}
+
+export default MyProps;
